9e0705f029c6923f0b22e1035f5949a3170964e9
aPesc <apescadorca23ziber@ikzubirimanteo.com>
2023-12-18 08:52:10 +0100

    prueba de respuesta generado

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-from flask import Flask, Jsonify, Request
+from flask import Flask, jsonify, request
 
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+
+def generateResponse():
+    if request.method == 'GET':
+        return "<h1>Hola esto es un texto generado de prueba</h1>"
+    
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
