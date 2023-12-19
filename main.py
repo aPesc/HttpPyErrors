cf26f9d89cd303f64b9c12fff391c1345f59c214
aPesc <apescadorca23ziber@ikzubirimanteo.com>
2023-12-19 09:30:42 +0100

    templates implementados

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
-from urllib import error
-from flask import Flask, request, redirect, url_for
+from flask import Flask, request, redirect, render_template, make_response
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
-def generateResponse():
-    
+@app.route('/', methods=['GET','POST'])
+def index():
     if request.method == 'GET':
-        return """<h1>INICIO</h1>
-                    <p>Lorem ipsum sir amet</p>
-                """
+        return render_template("index.html")
+    
+    if request.method == 'POST':
+        nombre = request.form.get("nombre")
+        asunto = request.form.get("asunto")
+        fecha = request.form.get("fecha")
+
+        #return make_response(f"<p>La incidencia en la fecha {fecha} a nombre de {nombre} con el siguiente asunto: {asunto}</p> <br> <p>Sera procesado</p>", 201)
+        return f"<p>La incidencia en la fecha {fecha} a nombre de {nombre} con el siguiente asunto: {asunto}</p> <br> <p>Sera procesado</p>"
 
 
-@app.route('/profile', methods=["GET"])
+
+
+"""@app.route('/profile', methods=["GET"])
 def profile():
     devices = ["iphone", "android"]
 
@@ -20,25 +26,26 @@ def profile():
     agent = request.headers.get("User-Agent")
 
     if devices[0] in agent.lower():
-        return """<p>Listado de perfiles</p>
+        return '''<p>Listado de perfiles</p>
                     <ul>
                         <li>Admin</li>
                         <li>Root</li>
-                    </ul>"""
+                    </ul>
+                '''
     else:
-        return redirect("\\")
+        return redirect("\\")"""
 
 @app.errorhandler(401)
 def err401(error):
-    return "<h2>Ups, ha sucedido algo raro</h2>"
+    return render_template("errcode.html")
 
 @app.errorhandler(403)
 def err403(error):
-    return "<h2>Ups, ha sucedido algo raro</h2>"
+    return render_template("errcode.html")
 
 @app.errorhandler(404)
 def err404(error):
-    return "<h2>Ups, ha sucedido algo raro</h2>"
+    return render_template("errcode.html")
 
 
 if __name__ == "__main__" :
